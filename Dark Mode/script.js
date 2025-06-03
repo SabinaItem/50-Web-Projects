00daa09950a75eee92d621ee42b7417abe31a094
SabinaItem <sabina@iteminc.com>
2025-06-03 14:31:12 -0400

    Dark Mode

---
+++ Dark Mode/script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container');
+const toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click', () => {
+  container.classList.toggle('dark')?(toggle.firstElementChild.className = 'fa-solid fa-moon'):(toggle.firstElementChild.className = 'fa-solid fa-sun');
+
+});
